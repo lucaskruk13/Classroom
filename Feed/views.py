6681cbc166aef9cc555c48c24c9dcf1d83542ebf
skywalker <Pf6m666yUBy0W1Apx7P7LYHlnm0g2V4T>
2019-02-18 14:44:14 -0600

    Add Redirect to the login page if the user is not authenticated

--- Feed/views.py
+++ Feed/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, redirect
 from django.views.generic import TemplateView
 from School.models import Class
+from django.http import HttpResponse
 
 class FeedView(TemplateView):
     template_name = 'feed/feed.html'
+
+    def render_to_response(self, context, **response_kwargs):
+        if not self.request.user.is_authenticated:
+            return redirect('login')
+
+        return super(FeedView, self).render_to_response(context)
+
 
     def get_context_data(self, **kwargs):
         # Call the base implementation to get a context
@@ -12,5 +20,7 @@ class FeedView(TemplateView):
         context['class_list'] = Class.objects.filter(profiles__user_id=self.request.user.id)
 
         return context
+
+
 
 
